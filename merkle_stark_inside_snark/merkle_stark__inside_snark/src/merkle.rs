d528c662b3d5373805435bb878ad00606d92b112
DoHoonKim <dohoon@pi-lab.io>
2023-01-04 14:58:10 +0900

    Test merkle tree circuit

--- merkle_stark_inside_snark/merkle_stark__inside_snark/src/merkle.rs
+++ merkle_stark_inside_snark/merkle_stark__inside_snark/src/merkle.rs
@@ -11,6 +11,7 @@ const D: usize = 2;
 type F = GoldilocksField;
 type Digest = [F; 4];
 
+#[derive(Clone)]
 struct MerkleTreeCircuitConfig {
     merkle_root: HashOutTarget,
     merkle_proof: MerkleProofTarget,
@@ -32,7 +33,7 @@ impl MerkleTreeCircuit {
         self.config.tree_height
     }
 
-    pub fn configure(&self, builder: &mut CircuitBuilder<F, D>, tree_height: usize) -> MerkleTreeCircuitConfig {
+    pub fn configure(builder: &mut CircuitBuilder<F, D>, tree_height: usize) -> MerkleTreeCircuitConfig {
         let merkle_root = builder.add_virtual_hash();
         builder.register_public_inputs(&merkle_root.elements);
 
@@ -92,5 +93,59 @@ impl MerkleTreeCircuit {
             public_key_index_target,
             F::from_canonical_usize(public_key_index),
         );
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Result;
+    use plonky2::field::field_types::Field;
+    use plonky2::hash::{poseidon::PoseidonHash, merkle_tree::MerkleTree};
+    use plonky2::iop::witness::PartialWitness;
+    use plonky2::plonk::circuit_builder::CircuitBuilder;
+    use plonky2::plonk::circuit_data::{CircuitConfig, CircuitData};
+    use plonky2::plonk::config::{Hasher, PoseidonGoldilocksConfig};
+    use crate::merkle::{
+        MerkleTreeCircuit,
+        D,
+        F,
+        Digest,
+    };
+
+    #[test]
+    fn merkle_test() -> Result<()> {
+        let n = 1 << 10;
+        let private_keys: Vec<Digest> = (0..n).map(|_| F::rand_arr()).collect();
+        let public_keys: Vec<Vec<F>> = private_keys
+            .iter()
+            .map(|&sk| {
+                PoseidonHash::hash_no_pad(&[sk, [F::ZERO; 4]].concat())
+                    .elements
+                    .to_vec()
+            })
+            .collect();
+        let merkle_tree: MerkleTree<F, PoseidonHash> = MerkleTree::new(public_keys, 0);
+
+        let public_key_index = 12;
+        let config = CircuitConfig::standard_recursion_zk_config();
+        let mut builder = CircuitBuilder::new(config);
+        let mut pw: PartialWitness<F> = PartialWitness::new();
+
+        let tree_height = 10;
+        let circuit_config = MerkleTreeCircuit::configure(&mut builder, tree_height);
+        let circuit = MerkleTreeCircuit::construct(circuit_config);
+        circuit.assign_targets(
+            &mut pw,
+            merkle_tree.cap.0[0],
+            merkle_tree.prove(public_key_index),
+            private_keys[public_key_index],
+            public_key_index,
+            circuit.config.clone(),
+        );
+
+        let data: CircuitData<F, PoseidonGoldilocksConfig, D> = builder.build();
+        let proof = data.prove(pw)?;
+
+        data.verify(proof)
     }
 }
