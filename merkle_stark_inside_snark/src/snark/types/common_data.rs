803cecbb50091ddb35e1fba872d0b2d54f47537a
DoHoonKim <dohoon@pi-lab.io>
2023-02-15 16:41:33 +0900

    Add types::common_data module

---
+++ merkle_stark_inside_snark/src/snark/types/common_data.rs
@@ -0,0 +1,107 @@
+use std::ops::Range;
+
+use crate::snark::gates::CustomGate;
+
+use super::to_goldilocks;
+use halo2curves::{goldilocks::fp::Goldilocks, FieldExt};
+use plonky2::{field::goldilocks_field::GoldilocksField, plonk::circuit_data::CommonCircuitData};
+
+#[derive(Debug, Default)]
+pub struct CircuitConfig {
+    pub num_wires: usize,
+    pub num_routed_wires: usize,
+    pub num_constants: usize,
+    /// Whether to use a dedicated gate for base field arithmetic, rather than using a single gate
+    /// for both base field and extension field arithmetic.
+    pub use_base_arithmetic_gate: bool,
+    pub security_bits: usize,
+    /// The number of challenge points to generate, for IOPs that have soundness errors of (roughly)
+    /// `degree / |F|`.
+    pub num_challenges: usize,
+    pub zero_knowledge: bool,
+    /// A cap on the quotient polynomial's degree factor. The actual degree factor is derived
+    /// systematically, but will never exceed this value.
+    pub max_quotient_degree_factor: usize,
+}
+
+#[derive(Debug, Default)]
+pub struct FriParams {
+    pub hiding: bool,
+    pub degree_bits: usize,
+    pub reduction_arity_bits: Vec<usize>,
+}
+
+#[derive(Default, Debug)]
+pub struct SelectorsInfo {
+    pub selector_indices: Vec<usize>,
+    pub groups: Vec<Range<usize>>,
+}
+
+#[derive(Default)]
+pub struct CommonData<F: FieldExt> {
+    pub config: CircuitConfig,
+
+    pub fri_params: FriParams,
+
+    /// The types of gates used in this circuit, along with their prefixes.
+    pub gates: Vec<Box<dyn CustomGate>>,
+
+    /// Information on the circuit's selector polynomials.
+    pub selectors_info: SelectorsInfo,
+
+    /// The degree of the PLONK quotient polynomial.
+    pub quotient_degree_factor: usize,
+
+    /// The largest number of constraints imposed by any gate.
+    pub num_gate_constraints: usize,
+
+    /// The number of constant wires.
+    pub num_constants: usize,
+
+    pub num_public_inputs: usize,
+
+    /// The `{k_i}` valued used in `S_ID_i` in Plonk's permutation argument.
+    pub k_is: Vec<F>,
+
+    /// The number of partial products needed to compute the `Z` polynomials.
+    pub num_partial_products: usize,
+}
+
+impl<F: FieldExt> CommonData<F> {
+    pub const fn degree_bits(&self) -> usize {
+        self.fri_params.degree_bits
+    }
+}
+
+impl From<CommonCircuitData<GoldilocksField, 2>> for CommonData<Goldilocks> {
+    fn from(value: CommonCircuitData<GoldilocksField, 2>) -> Self {
+        Self {
+            config: CircuitConfig {
+                num_wires: value.config.num_wires,
+                num_routed_wires: value.config.num_routed_wires,
+                num_constants: value.config.num_constants,
+                use_base_arithmetic_gate: value.config.use_base_arithmetic_gate,
+                security_bits: value.config.security_bits,
+                num_challenges: value.config.num_challenges,
+                zero_knowledge: value.config.zero_knowledge,
+                max_quotient_degree_factor: value.config.max_quotient_degree_factor,
+            },
+            // gates: value.gates,
+            fri_params: FriParams {
+                hiding: value.fri_params.hiding,
+                degree_bits: value.fri_params.degree_bits,
+                reduction_arity_bits: value.fri_params.reduction_arity_bits,
+            },
+            selectors_info: SelectorsInfo {
+                selector_indices: value.selectors_info.selector_indices,
+                groups: value.selectors_info.groups,
+            },
+            quotient_degree_factor: value.quotient_degree_factor,
+            num_gate_constraints: value.num_gate_constraints,
+            num_constants: value.num_constants,
+            num_public_inputs: value.num_public_inputs,
+            k_is: value.k_is.iter().map(|e| to_goldilocks(*e)).collect(),
+            num_partial_products: value.num_partial_products,
+        }
+    }
+}
--- merkle_stark_inside_snark/src/snark/types/mod.rs
+++ merkle_stark_inside_snark/src/snark/types/mod.rs
@@ -1,14 +1,13 @@
-use core::ops::Range;
 use halo2_proofs::circuit::Value;
 use halo2curves::{goldilocks::fp::Goldilocks, FieldExt};
 use plonky2::field::extension::Extendable;
-use plonky2::plonk::circuit_data::CommonCircuitData;
 use plonky2::{
     field::goldilocks_field::GoldilocksField,
     hash::{hash_types::HashOut, merkle_tree::MerkleCap, poseidon::PoseidonHash},
 };
 
 pub mod assigned;
+pub mod common_data;
 pub mod proof;
 pub mod verification_key;
 
@@ -72,93 +71,4 @@ pub fn to_extension_field_values(
         .iter()
         .map(|e| ExtensionFieldValue::from(e.0))
         .collect()
-}
-
-#[derive(Debug, Default)]
-pub struct CircuitConfig {
-    pub num_wires: usize,
-    pub num_routed_wires: usize,
-    pub num_constants: usize,
-    /// Whether to use a dedicated gate for base field arithmetic, rather than using a single gate
-    /// for both base field and extension field arithmetic.
-    pub use_base_arithmetic_gate: bool,
-    pub security_bits: usize,
-    /// The number of challenge points to generate, for IOPs that have soundness errors of (roughly)
-    /// `degree / |F|`.
-    pub num_challenges: usize,
-    pub zero_knowledge: bool,
-    /// A cap on the quotient polynomial's degree factor. The actual degree factor is derived
-    /// systematically, but will never exceed this value.
-    pub max_quotient_degree_factor: usize,
-}
-
-#[derive(Debug, Default)]
-pub struct FriParams {
-    pub hiding: bool,
-    pub degree_bits: usize,
-    pub reduction_arity_bits: Vec<usize>,
-}
-
-#[derive(Default, Debug)]
-pub struct SelectorsInfo {
-    pub selector_indices: Vec<usize>,
-    pub groups: Vec<Range<usize>>,
-}
-
-#[derive(Debug, Default)]
-pub struct CommonData<F: FieldExt> {
-    pub config: CircuitConfig,
-
-    pub fri_params: FriParams,
-
-    /// The types of gates used in this circuit, along with their prefixes.
-    /// pub(crate) gates: Vec<GateRef<F, D>>,
-
-    /// Information on the circuit's selector polynomials.
-    /// pub selectors_info: SelectorsInfo,
-
-    /// The degree of the PLONK quotient polynomial.
-    pub quotient_degree_factor: usize,
-
-    /// The largest number of constraints imposed by any gate.
-    pub num_gate_constraints: usize,
-
-    /// The number of constant wires.
-    pub num_constants: usize,
-
-    pub num_public_inputs: usize,
-
-    /// The `{k_i}` valued used in `S_ID_i` in Plonk's permutation argument.
-    pub k_is: Vec<F>,
-
-    /// The number of partial products needed to compute the `Z` polynomials.
-    pub num_partial_products: usize,
-}
-
-impl From<CommonCircuitData<GoldilocksField, 2>> for CommonData<Goldilocks> {
-    fn from(value: CommonCircuitData<GoldilocksField, 2>) -> Self {
-        Self {
-            config: CircuitConfig {
-                num_wires: value.config.num_wires,
-                num_routed_wires: value.config.num_routed_wires,
-                num_constants: value.config.num_constants,
-                use_base_arithmetic_gate: value.config.use_base_arithmetic_gate,
-                security_bits: value.config.security_bits,
-                num_challenges: value.config.num_challenges,
-                zero_knowledge: value.config.zero_knowledge,
-                max_quotient_degree_factor: value.config.max_quotient_degree_factor,
-            },
-            fri_params: FriParams {
-                hiding: value.fri_params.hiding,
-                degree_bits: value.fri_params.degree_bits,
-                reduction_arity_bits: value.fri_params.reduction_arity_bits,
-            },
-            quotient_degree_factor: value.quotient_degree_factor,
-            num_gate_constraints: value.num_gate_constraints,
-            num_constants: value.num_constants,
-            num_public_inputs: value.num_public_inputs,
-            k_is: value.k_is.iter().map(|e| to_goldilocks(*e)).collect(),
-            num_partial_products: value.num_partial_products,
-        }
-    }
 }
